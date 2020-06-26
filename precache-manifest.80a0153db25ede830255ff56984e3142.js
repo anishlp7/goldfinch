3878cd3e06d66773cbaf82ffccfcbdff9f578ad5
unknown <anishlp7@gmail.com>
2020-06-26 18:40:54 +0530

    Updates

---
+++ precache-manifest.80a0153db25ede830255ff56984e3142.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6add93280642bf1c29377be8f30e6fce",
+    "url": "/goldfinch/index.html"
+  },
+  {
+    "revision": "6df66c3264be1e918582",
+    "url": "/goldfinch/static/css/main.ea66e956.chunk.css"
+  },
+  {
+    "revision": "8f2ada87fab6bda8a32c",
+    "url": "/goldfinch/static/js/2.019d2262.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/goldfinch/static/js/2.019d2262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df66c3264be1e918582",
+    "url": "/goldfinch/static/js/main.46966a55.chunk.js"
+  },
+  {
+    "revision": "edabcf91ae31288c954f",
+    "url": "/goldfinch/static/js/runtime-main.9a6c1973.js"
+  },
+  {
+    "revision": "0f3dedb4599b5d198439d5d0dd1e64f3",
+    "url": "/goldfinch/static/media/bell.0f3dedb4.svg"
+  },
+  {
+    "revision": "038afd051e57659b1d6d276f5c565617",
+    "url": "/goldfinch/static/media/bg.038afd05.svg"
+  },
+  {
+    "revision": "52b166c3b73eca39dc62a4f7c251efd9",
+    "url": "/goldfinch/static/media/heroImage.52b166c3.svg"
+  },
+  {
+    "revision": "632d3a8dd691f912203b995bb00bb3d8",
+    "url": "/goldfinch/static/media/logo.632d3a8d.webp"
+  }
+]);
